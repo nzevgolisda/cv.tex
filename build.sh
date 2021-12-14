1c9d98e664be9a0a5baca7b3c716347dac4ddcda
nikos zevgolis <nzevgolisda@gmail.com>
2021-12-14 16:52:25 +0200

    diff.view + summary

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-pdflatex cv.tex && mv cv.pdf /mnt/c/Users/nikos/documents/job/cv/cv.zevgolis-nikolaos.$(date +%Y%m%d).pdf
+pdflatex cv.tex && mv cv.pdf /mnt/c/Users/USER/documents/job/cv/cv.zevgolis-nikolaos.$(date +%Y%m%d).pdf
